83d9df7f762cff82bb9d7c3c4d9f902fddcfac2d
Rizalmhmmd13zx <rizalmhmmd.13zx@gmail.com>
2021-04-16 06:00:48 +0800

    Downloading Gradle dan membuat FavoriteUser.kt (kosong)

---
+++ app/src/main/java/com/example/githubuserslist/FavoriteUser.kt
@@ -0,0 +1,11 @@
+package com.example.githubuserslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_user)
+    }
+}
